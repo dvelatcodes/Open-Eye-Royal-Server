d662d10736f49a9080f288689c7e8940d1702490
Love Olubummo <olubummolove3@gmail.com>
2022-10-26 23:11:45 +0100

    feature: added welcome endpoint

--- index.js
+++ index.js
@@ -3,23 +3,37 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 const app = express();
-// if (app.get("env") !== "production") 
+// if (app.get("env") !== "production")
 dotenv.config();
 const db = process.env.db;
 import router from "./routes/index.js";
 
-
 const PORT = process.env.PORT || 5000;
-app.use(express.json({limit : "50mb"}));
-app.use(express.urlencoded({extended : true}));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use("/",router);
+app.use("/", router);
 
-mongoose.connect(db, {useNewUrlParser : true}, {useUnifiedTopology : true}).then(()=>{
-    app.listen(PORT, ()=>{
-        console.log(`database connected successfully and server running on ${PORT}`)
-    })
-}).catch((err)=>{
-    console.log(err)
+// welcome endpoint
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to open royal api");
 });
+
+// wrong route endpoint
+app.get("*", (req, res) => {
+  res.status(404).send("404 Not found");
+});
+
+mongoose
+  .connect(db, { useNewUrlParser: true }, { useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(
+        `database connected successfully and server running on ${PORT}`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
