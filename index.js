fd76d7c1a600d765681e19bbc54e8faa0c6f86a1
Love Olubummo <olubummolove3@gmail.com>
2022-11-30 19:05:07 +0100

    fourth change on cors origin name

--- index.js
+++ index.js
@@ -9,10 +9,17 @@ const db = process.env.db;
 import router from "./routes/index.js";
 
 const PORT = process.env.PORT || 5000;
-app.use(cors({ origin: "*" }));
+// https://client-open-eye-royal.vercel.app/
 // https://client-open-eye-royal-dvelatcodes.vercel.app
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "https://client-open-eye-royal.vercel.app/",
+    methods: ["GET", "POST", "DELETE"],
+  })
+);
+// app.use(cors());
 
 app.use("/", router);
 
