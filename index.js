571d1983c1795af46bf76b7c39b2a6252df44045
Love Olubummo <olubummolove3@gmail.com>
2023-02-25 11:38:02 +0100

    fix:modified cors header

--- index.js
+++ index.js
@@ -13,13 +13,12 @@ const PORT = process.env.PORT || 5000;
 // https://client-open-eye-royal-dvelatcodes.vercel.app
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "DELETE"],
-  })
-);
-// app.use(cors());
+// app.use(
+//   cors({
+//     origin: "*"
+//   })
+// );
+app.use(cors());
 
 app.use("/", router);
 
