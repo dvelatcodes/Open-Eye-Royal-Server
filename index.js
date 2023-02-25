6b9a13238c1bc67cae867407ec56f096801bbf68
Love Olubummo <olubummolove3@gmail.com>
2023-02-25 12:07:09 +0100

    fix:added to access-control-allow-origin

--- index.js
+++ index.js
@@ -13,12 +13,20 @@ const PORT = process.env.PORT || 5000;
 // https://client-open-eye-royal-dvelatcodes.vercel.app
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
-// app.use(
-//   cors({
-//     origin: "*"
-//   })
-// );
 app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 app.use("/", router);
 
